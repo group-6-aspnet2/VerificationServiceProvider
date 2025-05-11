7a00f072b558ebc2add4199237ca18fe733a0aef
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2025-05-11 14:27:59 +0200

    Refactor verification request models
    - Updated `SaveVerificationCodeRequest` to include validation attributes for `Email` and `Code`, and added a `ValidFor` property.
    - Removed the unused `SaveVerificationRequest` class.
    - Introduced a new `SendVerificationRequest` class with an `Email` property.

--- WebApi/Models/SaveVerificationCodeRequest.cs
+++ WebApi/Models/SaveVerificationCodeRequest.cs
@@ -1,5 +1,13 @@
-﻿namespace WebApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models;
 
 public class SaveVerificationCodeRequest
 {
+    [Required]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    public string Code { get; set; } = null!;
+    public TimeSpan ValidFor { get; set; }
 }
