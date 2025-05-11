4e33330e9d94b7975b4e7d0e82b0c82218676cb8
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2025-05-11 14:36:50 +0200

    Enhance email functionality and middleware configuration
    - Added using directives for Azure Communication Email and WebApi Services.
    - Registered EmailClient as a singleton and added memory cache service.
    - Introduced transient service for IVerificationService.
    - Moved OpenAPI mapping to always execute, regardless of environment.
    - Implemented CORS middleware to allow all origins, headers, and methods.
    - Adjusted middleware order for authentication and authorization.
    - Removed app.Run() and ensured app.MapControllers() is called appropriately.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,22 +1,21 @@
+using Azure.Communication.Email;
+using WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
-
-
 builder.Services.AddControllers();
-
 builder.Services.AddOpenApi();
 
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSingleton(x => new EmailClient(builder.Configuration["ACS:ConnectionString"]));
+builder.Services.AddTransient<IVerificationService, VerificationService>();
+
 var app = builder.Build();
-
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
+app.MapOpenApi();
 app.UseHttpsRedirection();
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.Run();
